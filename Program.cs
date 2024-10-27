7ce6848cf791c2d2e1cdcdadbb1d33a963554987
muharrem <tarayanmuharrem@gmail.com>
2024-10-27 21:56:43 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,103 +4,130 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace foreach_döngü
+namespace _01_maainsubjectc_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region FOREACH DÖNGÜSÜ
-            //foreach(1;2;3;4)
-            //1-değişken türü
-            //2-değişken adı
-            //3-In
-            //4-liste, koleksiyon, dizi
+            #region YAZDIRMA KOMUTLARI
+            //Console.Write("merhaba dünya");
 
-            /*string[] cities = {"varsava", "Roma", "MİLANO", "london", "atina"};
-            foreach (string n in cities)
-            {
-                Console.WriteLine(n);
-            }*/
+            //Console.WriteLine("******Yemek Kategorileri*****");
+            //Console.WriteLine();
+            //Console.WriteLine("1-çorba");
+            //Console.WriteLine("2-ana yemek");
+            //Console.WriteLine("3-içecek");
+            //Console.WriteLine("4-ara sıcak");
+            //Console.WriteLine("5-tatlı");
+            //Console.WriteLine();
 
-            /*
-             int[] number = {1,2,45,541,852,1026,965,2,0,65,31};
-            foreach (int i in number)
-            {
-                if (i%2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }*/
+            #endregion
+            #region string DEĞİŞKENLER
+            //string
+            // değişkentürü+ + ;
 
-            //int[] numberSum = {1,3,5,7,9,11,13,15,17,19};
-            //int total = 0;
-            //foreach (int sum in numberSum)
-            //{
-            //    total += sum;
-            //}Console.WriteLine(total);
+            //string name;
+            //name = "muharrem";
+            //Console.Write(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+            //customerName = "ali";
+            //customerSurname = "veli";
+            //customerPhone = "0530123452785";
+            //customerEmail = "deneme@gmail.com";
+            //district = "korkuteli";
+            //city = "antalya";
+            //Console.WriteLine("**Rezervasyon kartı***");
+            //Console.WriteLine();
+            //Console.WriteLine("-------------------------------------");
+            //Console.WriteLine("müşteri:" + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişşim:"+customerPhone);
+            //Console.WriteLine("Email:" + customerEmail);
+            //Console.WriteLine("il:"+city+"/ ilçe: "+ district);
+
+            //Console.WriteLine();
+            //customerName = "murat";
+            //customerSurname = "turat";
+            //customerPhone = "0530123452785";
+            //customerEmail = "31bağımııs@gmail.com";
+            //district = "menteşe";
+            //city = "kapı";
+            //Console.WriteLine("-------------------------------------");
+            //Console.WriteLine("müşteri:" + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişşim:" + customerPhone);
+            //Console.WriteLine("Email:" + customerEmail);
+            //Console.WriteLine("il:" + city + "/ ilçe: " + district);
 
 
             #endregion
-            #region BURADAKİ SORUYU KENDİN ÇÖZCEKSİN
-            Console.WriteLine("***** C# Eğitim Kampı Sınav Uygulaması *****");
+            #region İNT DEĞİŞKENLER
+            // int 
+            int hamburgerPrice = 300;
+            int cokePrice = 40;
+            int waterPrice = 10;
+            int friesPrice = 50;
+            int pizzaPrice = 250;
+            int lemonadePrice = 30;
+
+            Console.WriteLine("*-* Restoran Menü Fiyatları*-*");
             Console.WriteLine();
+            Console.WriteLine("--- Hamburger:" + hamburgerPrice + "TL");
+            Console.WriteLine("--- Kola:" + cokePrice + "TL");
+            Console.WriteLine("--- Su:" + waterPrice + "TL");
+            Console.WriteLine("--- Patates Kızartması:" + friesPrice + "TL");
+            Console.WriteLine("--- Pizza:" + pizzaPrice + "TL");
+            Console.WriteLine("--- Limonata:" + lemonadePrice + "TL");
+
             Console.WriteLine();
+            int hamburgerCount;
+            int cokeCount;
+            int waterCount;
+            int friesCount;
+            int pizzaCount;
+            int lemonadeCount;
+
+            int totalhamburgerPrice = 0;
+            int totalcokePrice = 0;
+            int totalwaterPrice = 0;
+            int totalfriesPrice = 0;
+            int totalpizzaPrice = 0;
+            int totallemonadePrice = 0;
+
+
+
+            hamburgerCount = 3;
+            cokeCount = 3;
+            waterCount = 4;
+            friesCount = 6;
+            pizzaCount = 2;
+            lemonadeCount = 2;
+
+            totalhamburgerPrice = hamburgerCount * hamburgerPrice;
+            totalcokePrice = cokeCount * cokePrice;
+            totalwaterPrice = waterCount * waterPrice;
+            totalfriesPrice = friesCount * friesPrice;
+            totalpizzaPrice = pizzaCount * pizzaPrice;
+            totallemonadePrice = lemonadeCount * totalhamburgerPrice;
+            int totalinPrice = totalhamburgerPrice + totalwaterPrice + totalpizzaPrice + totalfriesPrice + totalcokePrice + totallemonadePrice;
+
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Hamburger fiyatı:" + totalhamburgerPrice + "TL");
+            Console.WriteLine("Kola fiyatı:" + totalcokePrice + "TL");
+            Console.WriteLine("Su fiyatı:" + totalwaterPrice + "TL");
+            Console.WriteLine("Kızartma fiyatı:" + totalfriesPrice + "TL");
+            Console.WriteLine("Pizza fiyatı:" + totalpizzaPrice + "TL");
+            Console.WriteLine("Limonata fiyatı:" + totallemonadePrice + "TL");
             Console.WriteLine();
+            Console.WriteLine("Toplam Tutar:" + totalinPrice + "TL");
 
-            //Sınıftaki öğrenci sayısını kullanıcıdan al
-            Console.WriteLine("----------------------------------");
-            Console.Write("Sınıfınızda Kaç Öğrenci var: ");
-            int studentCount = int.Parse(Console.ReadLine());// dönüşüm
-            Console.WriteLine("----------------------------------");
-
-            //Öğrenci isimleri ve not ortalamaları diziai
-            string[] studentNames = new string[studentCount];
-            double[] studentExamAvg = new double[studentCount];
-
-            for (int i = 0; i < studentCount; i++)
-            {
-                // sonra bir bak buraya şiraze kaydı
-                Console.Write($"{i + 1}. öğrencinin ismini giriniz: ");
-                studentNames[i] = Console.ReadLine();
-
-                double totalExamResult = 0;
-
-                // 3 sınav girişi
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write($"{studentNames[i]} adlı öğrencinin {j + 1}. sınav notunu giriniz: ");
-                    double value = double.Parse(Console.ReadLine());
-                    totalExamResult += value; 
-                }
-                Console.WriteLine();
-
-                studentExamAvg[i] = totalExamResult / 3;
-            }
-
-            
-            for (int i = 0; i < studentCount; i++)
-            {
-                Console.WriteLine("----------------------------------");
-
-                Console.WriteLine($"{studentNames[i]} adlı öğrencinin ortalaması: {studentExamAvg[i]}");// ortalaama
-
-                // burayı şekillendir sonra 
-                if (studentExamAvg[i] >= 50)
-                {
-                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersi geçti.");
-                }
-                else
-                {
-                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersten kaldı.");
-                }
-                Console.WriteLine("----------------------------------");
-                Console.WriteLine();
-            }
             #endregion
-
-
             Console.Read();
+
         }
     }
 }
